b53fadd5029fc4a2ddc0f0ba1e9e37d471f23356
dvss-dvss <kosevojdavid3@gmail.com>
2025-10-01 19:29:53 +0200

    Add Post model for admin registration

--- blog/models.py
+++ blog/models.py
@@ -8,26 +8,20 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
-
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    auythor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=2,
-        choices=Status.choices,
-        default=Status.DRAFT,
-    )
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta:
-        ordering = ('-publish',)
+        ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
         ]
 
     def __str__(self):
         return self.title
-    
